c90a13e0ed4d9ab58e87914298047d183d8199fa
josnif <josephnifemi89@gmail.com>
2024-03-15 18:38:30 +0100

    Add auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { UserButton } from "@clerk/nextjs";
+import { User } from '@clerk/backend';
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <p>Home</p>
+
+      <UserButton afterSignOutUrl='/' />
+    </div>
   )
 }
 
